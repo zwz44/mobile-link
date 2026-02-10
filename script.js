8c52946576f1039411453fef3b9b3b676a43fc15
zwz44 <zwz44999@gmail.com>
2026-02-10 06:14:48 +0300

    Add functions to start camera, get location, and device info

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
-alert("Script connected");
+function start(){
+  startCamera();
+  getLocation();
+  getDevice();
+}
+
+// Camera
+function startCamera(){
+  navigator.mediaDevices.getUserMedia({video:true})
+  .then(stream=>{
+    document.getElementById("video").srcObject = stream;
+  })
+  .catch(()=>{
+    alert("Camera denied");
+  });
+}
+
+// Location
+function getLocation(){
+  navigator.geolocation.getCurrentPosition(pos=>{
+    alert(
+      "Location:\n" +
+      pos.coords.latitude + ", " +
+      pos.coords.longitude
+    );
+  });
+}
+
+// Device info
+function getDevice(){
+  console.log(navigator.userAgent);
+}
